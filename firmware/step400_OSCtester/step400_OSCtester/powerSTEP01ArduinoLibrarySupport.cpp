d40db7f78669c0d2f5981c8d225d59b1968918a7
kaz saita <saita@kinoshita-lab.org>
2019-10-31 19:55:49 +0900

    [debug] SPIの速さを元に戻しました(4MHz)

--- firmware/step400_OSCtester/step400_OSCtester/powerSTEP01ArduinoLibrarySupport.cpp
+++ firmware/step400_OSCtester/step400_OSCtester/powerSTEP01ArduinoLibrarySupport.cpp
@@ -327,7 +327,7 @@ byte powerSTEP::SPIXfer(byte data)
   }
   dataPacket[_position] = data;
   digitalWrite(_CSPin, LOW);  
-  _SPI->beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE3));
+  _SPI->beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE3));
   _SPI->transfer(dataPacket, _numBoards);
   _SPI->endTransaction();
   digitalWrite(_CSPin, HIGH);
